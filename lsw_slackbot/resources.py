1069aee5046b30075db52e1735c33d0ca84d71c4
Emily Hunt <emily.hunt.physics@gmail.com>
2022-02-25 12:59:01 +0100

    memory measurement should be max, not mean!

--- lsw_slackbot/resources.py
+++ lsw_slackbot/resources.py
@@ -30,7 +30,7 @@ async def sample_resource_usage(data_dir: Path, filename: Optional[Union[str, Pa
     # Now we can combine the multiple measurements...
     dataframe = pd.concat(dataframe, ignore_index=True)
     dataframe = (dataframe.groupby("username")
-                 .agg({"cpu_percent": "mean", "memory": "mean", "threads": "mean"}))
+                 .agg({"cpu_percent": "mean", "memory": "max", "threads": "mean"}))
 
     dataframe['time'] = datetime.datetime.now()
 
